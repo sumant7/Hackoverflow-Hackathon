8d8d58219115431c9ee54e89b252d30175420b5a
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-04-07 14:26:48 +0530

    Registration page setup

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose  = require('mongoose');
 const bodyParser = require('body-parser');
-const register = require('./routes/register')
+const register = require('./routes/register');
 const app = express();
-const port = process.env.port || 3000 //process.env.port for hosting
+const port = process.env.port || 3000; //process.env.port for hosting
 
 app.set("view engine","ejs");
 
 app.use(express.static("public"));
 
-app.use('/', register)
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use('/', register);
 
 app.listen(port, ()=>{
-    console.log("Listening")
+    console.log("Listening");
 })
