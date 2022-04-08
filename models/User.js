0326e65e82039f1eac5625cc18df5d906490a5d4
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-04-08 13:14:49 +0530

    registration form begins

--- models/User.js
+++ models/User.js
@@ -1,17 +1,39 @@
+const { Timestamp } = require('mongodb')
 const mongoose  = require('mongoose')
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-    name: {
+    username: {
+        type: String, required: true, unique: true
+    },
+    password:{
+        type: String, default: false
+    },
+    name:{
         type: String, required: true
     },
-    isEndangered:{
-        type: Boolean, default: false
+    freedate: {
+        type: String, required: true
     },
-    entrydate: {
-        type: Date, default: Date.now()
+    from1: {
+        type: String, required: true, default: ""
+    },
+    till1: {
+        type: String, required: true, default: ""
+    },
+    from2: {
+        type: String, default: ""
+    },
+    till2: {
+        type: String, default: ""
+    },
+    from3: {
+        type: String, default: ""
+    },
+    till3: {
+        type: String, default: ""
     }
 })
 
-let item= mongoose.model('user', Schema)
+let item= mongoose.model('user', userSchema)
 module.exports = item
--- routes/register.js
+++ routes/register.js
@@ -2,8 +2,19 @@ const express = require('express');
 const router = express.Router();
 let message = ""
 router.get('/',(req,res)=>{
-    res.render('register.ejs',{title: "Register", message: message})
+    res.render('register.ejs',{title: "Register",  passerr: '',emailerr:''})
 })
 
+
+router.post('/',async (req,res)=>{
+    try{
+        const username = req.body.username;
+        const password = req.body.password;
+        const cpassword = req.body.cpassword;
+        
+    }catch(err){
+        res.render('register.ejs',{title: "Error",  passerr: '',emailerr:''})
+    }
+})
 
 module.exports = router;
