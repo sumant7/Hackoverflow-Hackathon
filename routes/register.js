f6b4dbc2bcea80f299e7a1d045d2cfb10bbca27b
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-04-08 14:53:57 +0530

    registratio authentication done

--- routes/register.js
+++ routes/register.js
@@ -2,38 +2,48 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/User')
-router.get('/',(req,res)=>{
-    res.render('register.ejs',{title: "Register",  passerr: '',emailerr:''})
+router.get('/', (req, res) => {
+    res.render('register.ejs', { title: "Register", passerr: '', usernameerr: '' })
 })
 
 
-router.post('/',async (req,res)=>{
-    try{
+router.post('/', async (req, res) => {
+    try {
         const username = req.body.username;
         const password = req.body.password;
         const cpassword = req.body.cpassword;
 
-        if(password===cpassword){
+        if (password === cpassword) {
+
             let newUser = new User({
                 username: username,
                 password: password,
                 from1: "",
-                till1:"",
-                freedate:"",
-                name:""
+                till1: "",
+                freedate: "",
+                name: ""
             })
             newUser.save()
-            .then((item)=>{
-                res.render('login.ejs',{title: "Done",  passerr: '',emailerr:''})
-            })
-            .catch(err=> console.log(err))
-            console.log("Ok")
-        }else{
-            res.render('register.ejs',{title: "Register",  passerr: 'Passwords not Matching',emailerr:''})
+                .then((item) => {
+                    res.render('login.ejs', { title: "Done", passerr: '', usernameerr: '' })
+                })
+                .catch(err=> console.log(err))
+            const testuser = await User.findOne({ username: username })
+            if(username === testuser.username)
+            {
+                res.render('register.ejs', { title: "", passerr: '', usernameerr: 'Username Already exits' })
+            }
+            else
+            {
+                console.log("Error")
+            }
+        } else {
+            res.render('register.ejs', { title: "Register", passerr: 'Passwords not Matching', usernameerr: '' })
         }
-    }catch(err){
-        res.render('register.ejs',{title: "Error",  passerr: '',emailerr:''})
+    } catch (err) {
+        res.render('register.ejs', { title: "Error", passerr: '', usernameerr: '' })
     }
 })
+
 
 module.exports = router;
